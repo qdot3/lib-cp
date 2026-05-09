c46e7c5f661f44bc9e3d6cdae2841024e4c9e239
qdot3 <quantum.engine.55g@gmail.com>
2026-05-09 17:51:46 +0900

    modified

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,11 +1,12 @@
-use std::io::{stdin, stdout, BufWriter};
+use std::io::stdin;
 
 use input::{bind, FastInput};
 use output::IntBuffer;
+use proconio::fastout;
 
+#[fastout]
 fn main() {
     let mut input: FastInput<std::io::StdinLock<'_>> = FastInput::new(stdin().lock());
-    let mut output = BufWriter::with_capacity(1 << 16, stdout().lock());
     let mut buf = IntBuffer::new();
 
     bind! { input >> n: usize, }
