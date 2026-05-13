7ddf1f75bd7fd3a3420d5ba0efb3fb2f48fac1ce
qdot3 <quantum.engine.55g@gmail.com>
2026-05-13 22:16:33 +0900

    modify

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -11,18 +11,4 @@ fn main() {
 
     bind! { input >> n: usize, mut tx: [(i32, i32); n], }
 
-    tx.iter_mut()
-        .for_each(|(t, x)| (*t, *x) = (*x + *t, *x - *t));
-    tx.sort_unstable_by_key(|(x, y)| (*x, -y));
-
-    let mut robot = Vec::with_capacity(n);
-    for (_, y) in tx {
-        let i = robot.partition_point(|v| *v < y);
-        if let Some(v) = robot.get_mut(i) {
-            *v = y
-        } else {
-            robot.push(y);
-        }
-    }
-    println!("{}", buf.format(robot.len()))
 }
