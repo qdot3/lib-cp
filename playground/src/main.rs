12821fd5f1f8c4eaa3e9f370ef9ee2711f3e615d
qdot3 <quantum.engine.55g@gmail.com>
2026-05-22 20:57:39 +0900

    modified

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,8 +1,10 @@
 use std::io::stdin;
 
 use output::IntBuffer;
+use proconio::fastout;
 use reader::FastBufReader;
 
+#[fastout]
 fn main() {
     let mut input = FastBufReader::<{ 1 << 16 }, _>::new(stdin().lock());
     let mut buf = IntBuffer::new();
